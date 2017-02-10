9ecc7874472d7666a26bb723d506ef42a004176b
zeticous <e0004327@u.nus.edu>
2017-02-10 16:49:37 +0800

    [T5A2] Completed Abstract & Polymorphism exercise.

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -15,7 +15,13 @@ public abstract class Command {
     protected AddressBook addressBook;
     protected List<? extends ReadOnlyPerson> relevantPersons;
     private int targetIndex = -1;
-
+    
+    /**
+     * alerts that a command mutates the address book.
+     * @return true if the command mutates
+     */
+    public abstract boolean isMutating();
+    
     /**
      * @param targetIndex last visible listing index of the target person
      */
