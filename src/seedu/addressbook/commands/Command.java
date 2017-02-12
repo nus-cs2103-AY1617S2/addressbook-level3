61db48bc60945a116b3f52546e934f7b4297b673
yufeng <e0003228@u.nus.edu>
2017-02-12 18:21:11 +0800

    Add a polymorphic isMutating method [LO-Polymorphism] Use abstract classes/methods [LO-Abstract] Add exception to an overridden method [LO-LSP]

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -39,8 +39,13 @@ public abstract class Command {
     /**
      * Executes the command and returns the result.
      */
-    public abstract CommandResult execute();
+    public abstract CommandResult execute() throws Exception;
 
+    /**
+     * Returns true if the command type mutate the data.
+     */
+    public abstract boolean isMutating();
+    
     /**
      * Supplies the data the command will operate on.
      */
