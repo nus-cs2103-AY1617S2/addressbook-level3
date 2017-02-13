b210a50176a688662ad1ce91a1a62128f370b933
Zihan Li <lizihan97@gmail.com>
2017-02-13 21:38:01 +0800

    Add a polymorphic isMutating method

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -65,4 +65,6 @@ public abstract class Command {
     public void setTargetIndex(int targetIndex) {
         this.targetIndex = targetIndex;
     }
+    
+    protected abstract boolean isMutating();
 }
