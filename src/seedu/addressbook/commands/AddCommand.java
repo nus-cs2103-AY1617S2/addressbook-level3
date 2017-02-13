416bb4ffabcbc6ba86c2e6b9cb90c110620fc274
Justin Wong <justinwong12337@gmail.com>
2017-02-13 14:18:58 +0800

    Add isMutating method to Command classes and modifying save logic

--- src/seedu/addressbook/commands/AddCommand.java
+++ src/seedu/addressbook/commands/AddCommand.java
@@ -48,7 +48,11 @@ public class AddCommand extends Command {
                 new UniqueTagList(tagSet)
         );
     }
-
+    
+    public boolean isMutating() {
+    	return true;
+    }
+    
     public AddCommand(Person toAdd) {
         this.toAdd = toAdd;
     }
--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -40,7 +40,12 @@ public abstract class Command {
      * Executes the command and returns the result.
      */
     public abstract CommandResult execute();
-
+    
+    /**
+     * Returns true for command types that mutate the data. e.g. AddCommand
+     */
+    public abstract boolean isMutating();
+    
     /**
      * Supplies the data the command will operate on.
      */
