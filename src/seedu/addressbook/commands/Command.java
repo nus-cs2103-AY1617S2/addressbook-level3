2dd36067acb5f2e55d2d971cb7176e40d4dcac25
Difeng Tang <Difeng Tang>
2017-02-14 19:04:02 +0800

    [LO-LSP] Command and AddCommand.java

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -39,7 +39,7 @@ public abstract class Command {
     /**
      * Executes the command and returns the result.
      */
-    public abstract CommandResult execute();
+    public abstract CommandResult execute() throws Exception;
 
     /**
      * returns true if data is manipulated
