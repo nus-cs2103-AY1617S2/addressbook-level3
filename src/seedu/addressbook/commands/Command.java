e589b143a6be494b6a2feb3a393da4c1d7393ba2
WUHEYANG\wuheyang0617 <wuheyang0617@gmail.com>
2017-02-14 22:29:01 +0800

    Use polymorphism and abstract classes/method

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -40,7 +40,12 @@ public abstract class Command {
      * Executes the command and returns the result.
      */
     public abstract CommandResult execute();
-
+    
+    /**
+     * Return true if the command modifies any data
+     */
+    public abstract boolean isMutating();
+    
     /**
      * Supplies the data the command will operate on.
      */
