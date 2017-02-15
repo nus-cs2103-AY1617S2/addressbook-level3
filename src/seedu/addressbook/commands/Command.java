6c09ea447019a3b3ee4a04c6615ff3106d477519
Amro Shohoud <amro@73-152.priv23.nus.edu.sg>
2017-02-15 15:49:07 +0800

    added throws Exception clause

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -39,7 +39,7 @@ public abstract class Command {
     /**
      * Executes the command and returns the result.
      */
-    public abstract CommandResult execute();
+    public abstract CommandResult execute() throws Exception;
     
     /**
     * @return true if command modifies data and false if not.
