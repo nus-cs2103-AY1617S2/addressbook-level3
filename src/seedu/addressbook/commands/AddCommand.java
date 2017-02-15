3cd1f39b207d51f1a6b8ed0ab25d2caf9c8a7fcd
FausticSun <FausticSun@gmail.com>
2017-02-15 12:29:45 +0800

    Throw exception when addComand fails

--- src/seedu/addressbook/commands/AddCommand.java
+++ src/seedu/addressbook/commands/AddCommand.java
@@ -58,12 +58,12 @@ public class AddCommand extends Command {
     }
 
     @Override
-    public CommandResult execute() {
+    public CommandResult execute() throws Exception {
         try {
             addressBook.addPerson(toAdd);
             return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
         } catch (UniquePersonList.DuplicatePersonException dpe) {
-            return new CommandResult(MESSAGE_DUPLICATE_PERSON);
+            throw new Exception(MESSAGE_DUPLICATE_PERSON);
         }
     }
 
