f9894fa5f7d18bbbd31629428cbe18b533adac82
Melvin <melvin0719@hotmail.com>
2017-02-16 06:31:49 +0800

    Add app icon

--- src/seedu/addressbook/ui/Gui.java
+++ src/seedu/addressbook/ui/Gui.java
@@ -2,11 +2,11 @@ package seedu.addressbook.ui;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import seedu.addressbook.logic.Logic;
 import seedu.addressbook.Main;
 
-import java.io.File;
 import java.io.IOException;
 
 /**
@@ -19,6 +19,7 @@ public class Gui {
 
     public static final int INITIAL_WINDOW_WIDTH = 800;
     public static final int INITIAL_WINDOW_HEIGHT = 600;
+    public static final String ICON_FILENAME = "Apps-addressbook-icon.png";
     private final Logic logic;
 
     private MainWindow mainWindow;
@@ -42,6 +43,7 @@ public class Gui {
          */
         loader.setLocation(Main.class.getResource("ui/mainwindow.fxml"));
 
+        stage.getIcons().add(new Image(getClass().getResourceAsStream(ICON_FILENAME)));
         stage.setTitle(version);
         stage.setScene(new Scene(loader.load(), INITIAL_WINDOW_WIDTH, INITIAL_WINDOW_HEIGHT));
         stage.show();
