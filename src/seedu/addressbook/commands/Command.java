2137efb7f59ef6dc4fd2f3638df010c30f099dfe
Ali Rizvi <Ali Rizvi>
2017-02-17 06:31:14 -1200

    Added isMutating() to Command.java and subclasses

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -40,6 +40,11 @@ public abstract class Command {
      * Executes the command and returns the result.
      */
     public abstract CommandResult execute();
+    
+    /**
+     * Returns true for command types that mutate the data
+     */
+    public abstract boolean isMutating();
 
     /**
      * Supplies the data the command will operate on.
