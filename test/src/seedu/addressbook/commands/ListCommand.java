ba5da0cf6e61d7063750e24322fa19734b319daa
chrishendra93 <chrishendra93@gmail.com>
2017-02-21 13:29:11 +0800

    Solve raised issues

---
+++ test/src/seedu/addressbook/commands/ListCommand.java
@@ -0,0 +1,32 @@
+package seedu.addressbook.commands;
+
+import seedu.addressbook.data.person.ReadOnlyPerson;
+
+import java.util.List;
+
+
+/**
+ * Lists all persons in the address book to the user.
+ */
+public class ListCommand extends Command {
+
+    public static final String COMMAND_WORD = "list";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n" 
+            + "Displays all persons in the address book as a list with index numbers.\n\t"
+            + "Example: " + COMMAND_WORD;
+
+
+    @Override
+    public CommandResult execute() {
+        List<ReadOnlyPerson> allPersons = addressBook.getAllPersons().immutableListView();
+        return new CommandResult(getMessageForPersonListShownSummary(allPersons), allPersons);
+    }
+
+
+	@Override
+	public boolean isMutating() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
