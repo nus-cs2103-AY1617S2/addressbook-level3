fb5184556a62620664dbea5974ff4b7c58e4ff2f
Lee Jin Shun <ljsrockz@gmail.com>
2017-02-21 22:45:20 +0800

    Add isMutating Method

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -40,6 +40,12 @@ public abstract class Command {
      * Executes the command and returns the result.
      */
     public abstract CommandResult execute();
+    
+    /**
+     * Returns true for commands that mutates the addressbook data structure
+     */
+    public abstract boolean isMutating();
+	  
 
     /**
      * Supplies the data the command will operate on.
