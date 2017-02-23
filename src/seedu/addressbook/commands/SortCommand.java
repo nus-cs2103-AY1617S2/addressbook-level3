16cc6319730f287bec5f203ac53abfb3e002c605
sherinatoh <sherinatohh@gmail.com>
2017-02-24 04:00:59 +0800

    add static variables COMMAND_WORD and MESSAGE_USAGE to SortCommand

--- src/seedu/addressbook/commands/SortCommand.java
+++ src/seedu/addressbook/commands/SortCommand.java
@@ -2,5 +2,11 @@ package seedu.addressbook.commands;
 
 public class SortCommand extends Command{
 
+	public static final String COMMAND_WORD = "sort";
+	
+	public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n" + "Sorts all persons in addressbook according to name"
+			+ "and displays them as a list with index numbers.\n\t"
+			+ "Example: " + COMMAND_WORD;
+	
 
 }
