fa4071410bb7c67fa32804fc95d97c3f06c9917b
Shannon <shannonyongjs@gmail.com>
2017-02-24 10:37:21 +0800

    enhance find command to make it case insensitive and change relevant junit test cases

--- src/seedu/addressbook/commands/FindCommand.java
+++ src/seedu/addressbook/commands/FindCommand.java
@@ -38,15 +38,19 @@ public class FindCommand extends Command {
 
     /**
      * Retrieve all persons in the address book whose names contain some of the specified keywords.
-     *
+     * The command is case insensitive
      * @param keywords for searching
      * @return list of persons found
      */
     private List<ReadOnlyPerson> getPersonsWithNameContainingAnyKeyword(Set<String> keywords) {
         final List<ReadOnlyPerson> matchedPersons = new ArrayList<>();
+        Set<String> temp = new HashSet<String>();
+        for (String each: keywords){
+            temp.add(each.toLowerCase());
+        }
         for (ReadOnlyPerson person : addressBook.getAllPersons()) {
-            final Set<String> wordsInName = new HashSet<>(person.getName().getWordsInName());
-            if (!Collections.disjoint(wordsInName, keywords)) {
+            final Set<String> wordsInName1 = new HashSet<>(person.getName().getWordsInNameLowercase());
+            if (!Collections.disjoint(wordsInName1, temp)) {
                 matchedPersons.add(person);
             }
         }
