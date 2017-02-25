38660d261b6d768745a8be0dd22f7ad487c56cce
Tan Jian Sin <e0003810@u.nus.edu>
2017-02-25 23:29:31 +0800

    Added Partial Match from Start Added Near Match using Levenshtein Distance algorithm with error margin of 2

--- src/seedu/addressbook/commands/FindCommand.java
+++ src/seedu/addressbook/commands/FindCommand.java
@@ -50,14 +50,48 @@ public class FindCommand extends Command {
         }
         for (ReadOnlyPerson person : addressBook.getAllPersons()) {
             final Set<String> wordsInName = new HashSet<String>();
-            for(String w : person.getName().getWordsInName()){
+            for(String w : person.getName().getWordsInName()) {
                 wordsInName.add(w.toLowerCase());
             }
+            /* Exact Match */
             if (!Collections.disjoint(wordsInName, lowerCaseKeywords)) {
                 matchedPersons.add(person);
             }
+            /* Partial Match from Start 
+             * Near Match (Error Margin Allowed: 2)*/
+            for(String k: lowerCaseKeywords) {
+                for(String w : wordsInName) {
+                    if(w.startsWith(k)) {
+                        matchedPersons.add(person);
+                    }else if(computeLevenshteinDistance(w,k) <= 2) {
+                        matchedPersons.add(person);
+                    }
+                }
+            }
+            
         }
         return matchedPersons;
     }
-
+    private int computeLevenshteinDistance(CharSequence str1, CharSequence str2 ) {
+        int[][] distance = new int[str1.length() + 1][str2.length() + 1];
+    
+        for (int i = 0; i <= str1.length(); i++)
+           distance[i][0] = i;
+        for (int j = 1; j <= str2.length(); j++)
+           distance[0][j] = j;
+    
+        for (int i = 1; i <= str1.length(); i++)
+           for (int j = 1; j <= str2.length(); j++)
+              distance[i][j] =
+                 minimum(
+                    distance[i - 1][j] + 1,
+                    distance[i][j - 1] + 1,
+                    distance[i - 1][j - 1] +
+                        ((str1.charAt(i - 1) == str2.charAt(j - 1)) ? 0 : 1));
+    
+        return distance[str1.length()][str2.length()];
+    }
+    private int minimum(int a, int b, int c) {
+        return Math.min(Math.min(a, b), c);
+    }
 }
