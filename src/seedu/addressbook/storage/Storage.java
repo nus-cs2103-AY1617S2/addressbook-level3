ecc153b72299ba229bcb480df4652d1589454955
Shermine Jong <shermine_jong_ruth_93@outlook.sg>
2017-02-26 13:33:28 +0800

    T6A1DIP completed

---
+++ src/seedu/addressbook/storage/Storage.java
@@ -0,0 +1,30 @@
+package seedu.addressbook.storage;
+
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.storage.Storage.InvalidStorageFilePathException;
+import seedu.addressbook.storage.Storage.StorageOperationException;
+
+public abstract class Storage {
+	public static class StorageOperationException extends Exception {
+        public StorageOperationException(String message) {
+            super(message);
+        }
+    }
+	public static class InvalidStorageFilePathException extends IllegalValueException {
+        public InvalidStorageFilePathException(String message) {
+            super(message);
+        }
+    }
+	
+	public abstract AddressBook load() throws StorageOperationException;
+	
+	public abstract String getPath();
+	
+	public abstract void save(AddressBook addressBook) throws StorageOperationException;
+
+	public static Storage initialiseStorage() throws InvalidStorageFilePathException{
+		return new StorageFile();		
+	}
+	
+}
