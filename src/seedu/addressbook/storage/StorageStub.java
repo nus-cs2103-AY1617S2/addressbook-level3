f6a7fcd3aaa11e656e4731fc8a9383539267a6bb
A0162011A <e0112235@u.nus.edu>
2017-02-26 17:22:02 +0800

    Add Storage Class and StorageStub Class

---
+++ src/seedu/addressbook/storage/StorageStub.java
@@ -0,0 +1,31 @@
+package seedu.addressbook.storage;
+
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.storage.StorageFile.StorageOperationException;
+
+public class StorageStub extends Storage{
+
+    public StorageStub(String path) {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public AddressBook load() throws StorageOperationException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getPath() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void save(AddressBook addressBook) throws StorageOperationException {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
