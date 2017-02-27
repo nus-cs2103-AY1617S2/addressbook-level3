3e0da46a2ddffe9020a04c1e027078cab7c86c92
Joseph Tang <josephtang765@gmail.com>
2017-02-27 22:39:03 +0800

    Correct findtag invalid command format message

--- src/seedu/addressbook/parser/Parser.java
+++ src/seedu/addressbook/parser/Parser.java
@@ -243,7 +243,7 @@ public class Parser {
         final Matcher matcher = KEYWORD_ARGS_FORMAT.matcher(args.trim());
         if (!matcher.matches()) {
             return new IncorrectCommand(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    FindCommand.MESSAGE_USAGE));
+                    FindTagCommand.MESSAGE_USAGE));
         }
 
         // keywords delimited by whitespace
