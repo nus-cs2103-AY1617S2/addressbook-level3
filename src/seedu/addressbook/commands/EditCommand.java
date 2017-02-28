c89797998e8e5007d4937f3c8e6adac3e46e38c4
Denver Tan <revned.nat90@gmail.com>
2017-03-01 02:41:29 +0800

    Add EditCommand and EditCommandParser classes

---
+++ src/seedu/addressbook/commands/EditCommand.java
@@ -0,0 +1,178 @@
+package seedu.addressbook.commands;
+
+import seedu.addressbook.common.Utils;
+import seedu.addressbook.common.Messages;
+import seedu.addressbook.data.person.UniquePersonList;
+import seedu.addressbook.commands.CommandResult;
+import seedu.addressbook.data.person.Person;
+import seedu.addressbook.data.person.ReadOnlyPerson;
+import seedu.addressbook.data.person.Address;
+import seedu.addressbook.data.person.Email;
+import seedu.addressbook.data.person.Name;
+import seedu.addressbook.data.person.Phone;
+import seedu.addressbook.data.tag.UniqueTagList;
+
+import java.util.Optional;
+
+public class EditCommand extends Command{
+
+    public static final String COMMAND_WORD = "edit";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Edits the details of the person identified "
+            + "by the index number used in the last person listing. "
+            + "Existing values will be overwritten by the input values.\n"
+            + "Parameters: INDEX (must be a positive integer) [NAME] [p/PHONE] [e/EMAIL] [a/ADDRESS ] [t/TAG]...\n"
+            + "Example: " + COMMAND_WORD + " 1 p/91234567 e/johndoe@yahoo.com";
+
+    public static final String MESSAGE_EDIT_PERSON_SUCCESS = "Edited Person: %1$s";
+    public static final String MESSAGE_NOT_EDITED = "At least one field to edit must be provided.";
+    public static final String MESSAGE_DUPLICATE_PERSON = "This person already exists in the address book.";
+
+    private final int filteredPersonListIndex;
+    private final EditPersonDescriptor editPersonDescriptor;
+    
+    /**
+     * @param filteredPersonListIndex the index of the person in the filtered person list to edit
+     * @param editPersonDescriptor details to edit the person with
+     */
+    public EditCommand(int filteredPersonListIndex, EditPersonDescriptor editPersonDescriptor) {
+        super(filteredPersonListIndex);
+        assert filteredPersonListIndex > 0;
+        assert editPersonDescriptor != null;
+
+        // converts filteredPersonListIndex from one-based to zero-based.
+        this.filteredPersonListIndex = filteredPersonListIndex -1 ;
+
+        this.editPersonDescriptor = new EditPersonDescriptor(editPersonDescriptor);
+    }
+
+    @Override
+    public CommandResult execute() {
+        ReadOnlyPerson personToEdit = getTargetPerson();
+        if (!addressBook.containsPerson(personToEdit)) {
+            return new CommandResult(Messages.MESSAGE_PERSON_NOT_IN_ADDRESSBOOK);
+        }
+        Person editedPerson = createEditedPerson(personToEdit, editPersonDescriptor);
+        try {
+            addressBook.updatePerson(filteredPersonListIndex, editedPerson);
+        } catch (UniquePersonList.DuplicatePersonException dpe) {
+            return new CommandResult(Messages.MESSAGE_DUPLICATE_PERSON);
+        }
+        return new CommandResult(String.format(MESSAGE_EDIT_PERSON_SUCCESS, personToEdit));
+    }
+    
+    
+    
+    
+//    @Override
+//    public void redo() throws Exception {
+//        try {
+//            addressBook.addPerson(p1);
+//        } catch (UniquePersonList.DuplicatePersonException e) {
+//            // TODO Auto-generated catch block
+//            throw new Exception("RedoException");
+//        }
+//    }
+//
+//    @Override
+//    public void undo() throws Exception {
+//        try {
+//            addressBook.removePerson(p1);
+//        } catch (PersonNotFoundException e) {
+//            // TODO Auto-generated catch block
+//            throw new Exception("UndoException");
+//        }
+//    }
+
+    /**
+     * Creates and returns a {@code Person} with the details of {@code personToEdit}
+     * edited with {@code editPersonDescriptor}.
+     */
+    private static Person createEditedPerson(ReadOnlyPerson personToEdit,
+                                             EditPersonDescriptor editPersonDescriptor) {
+        assert personToEdit != null;
+
+        Name updatedName = editPersonDescriptor.getName().orElseGet(personToEdit::getName);
+        Phone updatedPhone = editPersonDescriptor.getPhone().orElseGet(personToEdit::getPhone);
+        Email updatedEmail = editPersonDescriptor.getEmail().orElseGet(personToEdit::getEmail);
+        Address updatedAddress = editPersonDescriptor.getAddress().orElseGet(personToEdit::getAddress);
+        UniqueTagList updatedTags = editPersonDescriptor.getTags().orElseGet(personToEdit::getTags);
+
+        return new Person(updatedName, updatedPhone, updatedEmail, updatedAddress, updatedTags);
+    }
+    
+    /**
+     * Stores the details to edit the person with. Each non-empty field value will replace the
+     * corresponding field value of the person.
+     */
+    public static class EditPersonDescriptor {
+        private Optional<Name> name = Optional.empty();
+        private Optional<Phone> phone = Optional.empty();
+        private Optional<Email> email = Optional.empty();
+        private Optional<Address> address = Optional.empty();
+        private Optional<UniqueTagList> tags = Optional.empty();
+
+        public EditPersonDescriptor() {}
+
+        public EditPersonDescriptor(EditPersonDescriptor toCopy) {
+            this.name = toCopy.getName();
+            this.phone = toCopy.getPhone();
+            this.email = toCopy.getEmail();
+            this.address = toCopy.getAddress();
+            this.tags = toCopy.getTags();
+        }
+
+        /**
+         * Returns true if at least one field is edited.
+         */
+        public boolean isAnyFieldEdited() {
+            return Utils.isAnyPresent(this.name, this.phone, this.email, this.address, this.tags);
+        }
+
+        public void setName(Optional<Name> name) {
+            assert name != null;
+            this.name = name;
+        }
+
+        public Optional<Name> getName() {
+            return name;
+        }
+
+        public void setPhone(Optional<Phone> phone) {
+            assert phone != null;
+            this.phone = phone;
+        }
+
+        public Optional<Phone> getPhone() {
+            return phone;
+        }
+
+        public void setEmail(Optional<Email> email) {
+            assert email != null;
+            this.email = email;
+        }
+
+        public Optional<Email> getEmail() {
+            return email;
+        }
+
+        public void setAddress(Optional<Address> address) {
+            assert address != null;
+            this.address = address;
+        }
+
+        public Optional<Address> getAddress() {
+            return address;
+        }
+
+        public void setTags(Optional<UniqueTagList> tags) {
+            assert tags != null;
+            this.tags = tags;
+        }
+
+        public Optional<UniqueTagList> getTags() {
+            return tags;
+        }
+    }
+
+}
