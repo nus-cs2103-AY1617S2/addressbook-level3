87c744e6ead7399d0b35c83d9907fdf7d3bdc991
Peixuan <lee.pei.xuan.2011@vjc.sg>
2017-02-28 20:43:25 +0800

    implement StorageStub with empty save() method update LogicTest to work with StorageStub

---
+++ src/seedu/addressbook/storage/StorageStub.java
@@ -0,0 +1,29 @@
+package seedu.addressbook.storage;
+
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.storage.StorageFile.StorageOperationException;
+
+public class StorageStub extends Storage {
+
+	private String path;
+	private AddressBook addressBook;
+	
+	public StorageStub(String path) {
+		this.path = path;
+	}
+	
+	@Override
+	public void save(AddressBook addressBook) throws StorageOperationException {
+	}
+
+	@Override
+	public AddressBook load() throws StorageOperationException {
+		return this.addressBook;
+	}
+
+	@Override
+	public String getPath() {
+		return path.toString();  
+	}
+	
+}
