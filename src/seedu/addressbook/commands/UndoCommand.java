1de153f9d0ad6cd486520e5e483a1de35aebec9f
ang-zy <angzy95@yahoo.com.sg>
2017-02-28 18:32:32 +0800

    Create UndoCommand

--- src/seedu/addressbook/commands/UndoCommand.java
+++ src/seedu/addressbook/commands/UndoCommand.java
@@ -1,28 +1,39 @@
 package seedu.addressbook.commands;
 
-import seedu.addressbook.commands.exception.UndoFailedException;
+import seedu.addressbook.commands.exception.*;
 
 public class UndoCommand extends Command {
+    
+    public static final String COMMAND_WORD = "undo";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n" + "Undoes the last change to the address book.\n\t"
+            + "Example: " + COMMAND_WORD;
 
-    public UndoCommand(int targetIndex) {
-        super(targetIndex);
-        // TODO Auto-generated constructor stub
-    }
-
-    public UndoCommand() {
-        // TODO Auto-generated constructor stub
-    }
+    public static final String MESSAGE_SUCCESS = "Undone: %s";
+    public static final String MESSAGE_EMPTY_HISTORY = "Nothing to undo!";
+    public static final String MESSAGE_FAILURE = "Failed to undo: %s";
 
     @Override
     public CommandResult execute() {
         // TODO Auto-generated method stub
-        return null;
+        while (!commandHistory.isEmpty()) {
+            Command lastCommand = commandHistory.pop();
+            if (!lastCommand.isUndoable()){
+                continue;
+            } else {
+                try {
+                    String undoResult = lastCommand.undo();
+                    return new CommandResult(String.format(MESSAGE_SUCCESS, undoResult));
+                } catch (UndoFailedException e) {
+                    return new CommandResult(String.format(MESSAGE_FAILURE, e.getMessage()));
+                }
+            }
+        }
+        return new CommandResult(MESSAGE_EMPTY_HISTORY);
     }
 
     @Override
     public boolean isMutating() {
-        // TODO Auto-generated method stub
-        return false;
+        return true;
     }
 
     @Override
@@ -32,9 +43,9 @@ public class UndoCommand extends Command {
     }
 
     @Override
-    public CommandResult undo() throws UndoFailedException {
+    public String undo() throws UndoFailedException {
         // TODO Auto-generated method stub
-        return null;
+        throw new IllegalUndoOperationException(COMMAND_WORD);
     }
 
 }
