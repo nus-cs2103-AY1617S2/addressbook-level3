e5fbe92315bb4769ebc4f844df09141adae4d2ed
ang-zy <angzy95@yahoo.com.sg>
2017-02-28 16:36:11 +0800

    Create class UndoFailedException

---
+++ src/seedu/addressbook/commands/exception/UndoFailedException.java
@@ -0,0 +1,14 @@
+package seedu.addressbook.commands.exception;
+
+/**
+ * Signals that the undo operation has failed
+ */
+public class UndoFailedException extends Exception {
+
+    public UndoFailedException(String message) {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+
+
+}
