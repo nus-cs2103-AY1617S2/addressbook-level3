94b1e5d24cc6fd8c0168d6e76bc7d6a33c31af2c
LOH ZI BIN ROBIN <robinloh@u.nus.edu>
2017-02-28 23:23:33 +0800

    Add getMessageForPersonSortedSummary in Command.java; Add MESSAGE_PERSONS_SORTED_OVERVIEW in Messages.java

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -35,6 +35,16 @@ public abstract class Command {
     public static String getMessageForPersonListShownSummary(List<? extends ReadOnlyPerson> personsDisplayed) {
         return String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, personsDisplayed.size());
     }
+    
+    /**
+     * Constructs a feedback message to summarise an operation that sorted a listing of persons.
+     *
+     * @param personsDisplayed used to generate summary
+     * @return summary message for persons sorted
+     */
+    public static String getMessageForPersonSortedShownSummary(List<? extends ReadOnlyPerson> personsDisplayed) {
+        return String.format(Messages.MESSAGE_PERSONS_SORTED_OVERVIEW, personsDisplayed.size());
+    }
 
     /**
      * Executes the command and returns the result.
