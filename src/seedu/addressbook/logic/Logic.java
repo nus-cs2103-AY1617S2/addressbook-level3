fefb717e45c9f8af18e12cfa559accec5f7eaa62
ruolan <fionaxrl@gmail.com>
2017-03-01 00:00:17 +0800

    Invert dependency from Logic to Storage update Logic, StorageFile, Gui

--- src/seedu/addressbook/logic/Logic.java
+++ src/seedu/addressbook/logic/Logic.java
@@ -6,6 +6,7 @@ import seedu.addressbook.data.AddressBook;
 import seedu.addressbook.data.person.ReadOnlyPerson;
 import seedu.addressbook.parser.Parser;
 import seedu.addressbook.storage.StorageFile;
+import seedu.addressbook.storage.Storage;
 
 import java.util.Collections;
 import java.util.List;
@@ -17,7 +18,7 @@ import java.util.Optional;
 public class Logic {
 
 
-    private StorageFile storage;
+    private Storage storage;
     private AddressBook addressBook;
 
     /** The list of person shown to the user most recently.  */
@@ -28,12 +29,12 @@ public class Logic {
         setAddressBook(storage.load());
     }
 
-    Logic(StorageFile storageFile, AddressBook addressBook){
+    Logic(Storage storageFile, AddressBook addressBook){
         setStorage(storageFile);
         setAddressBook(addressBook);
     }
 
-    void setStorage(StorageFile storage){
+    void setStorage(Storage storage){
         this.storage = storage;
     }
 
@@ -43,13 +44,14 @@ public class Logic {
 
     /**
      * Creates the StorageFile object based on the user specified path (if any) or the default storage path.
-     * @throws StorageFile.InvalidStorageFilePathException if the target file path is incorrect.
+     * @throws StorageFile.InvalidStorageFilePathException 
+     * 					if the target file path is incorrect.
      */
-    private StorageFile initializeStorage() throws StorageFile.InvalidStorageFilePathException {
+    private Storage initializeStorage() throws StorageFile.InvalidStorageFilePathException {
         return new StorageFile();
     }
 
-    public String getStorageFilePath() {
+    public String getStoragePath() {
         return storage.getPath();
     }
 
@@ -66,7 +68,8 @@ public class Logic {
 
     /**
      * Parses the user command, executes it, and returns the result.
-     * @throws Exception if there was any problem during command execution.
+     * @throws Exception 
+     * 				if there was any problem during command execution.
      */
     public CommandResult execute(String userCommandText) throws Exception {
         Command command = new Parser().parseCommand(userCommandText);
@@ -80,7 +83,8 @@ public class Logic {
      *
      * @param command user command
      * @return result of the command
-     * @throws Exception if there was any problem during command execution.
+     * @throws Exception 
+     * 				if there was any problem during command execution.
      */
     private CommandResult execute(Command command) throws Exception {
         command.setData(addressBook, lastShownList);
