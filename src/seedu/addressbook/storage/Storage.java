1664a94701a64bac89eff7336cb49520169b0099
Zhang Hanming <zhang.hanming.official@gmail.com>
2017-03-01 21:24:09 +0800

    change dependency of StorageFile and Logic

--- src/seedu/addressbook/storage/Storage.java
+++ src/seedu/addressbook/storage/Storage.java
@@ -1,10 +1,30 @@
 package seedu.addressbook.storage;
 
 import seedu.addressbook.data.AddressBook;
-import seedu.addressbook.storage.StorageFile.StorageOperationException;
+import seedu.addressbook.data.exception.IllegalValueException;
 
-public interface Storage {
-	public void save(AddressBook addressBook) throws StorageOperationException;
-	public AddressBook load() throws StorageOperationException;
-	public String getPath();
+public abstract class Storage {
+    
+    /**
+     * Signals that some error has occured while trying to convert and read/write data between the application
+     * and the storage file.
+     */
+    public static class StorageOperationException extends Exception {
+        public StorageOperationException(String message) {
+            super(message);
+        }
+    }
+    
+    /**
+     * Signals that the given file path does not fulfill the storage filepath constraints.
+     */
+    public static class InvalidStorageFilePathException extends IllegalValueException {
+        public InvalidStorageFilePathException(String message) {
+            super(message);
+        }
+    }
+    
+    public abstract void save(AddressBook addressBook) throws StorageOperationException;
+    public abstract AddressBook load() throws StorageOperationException;
+    public abstract String getPath();   
 }
