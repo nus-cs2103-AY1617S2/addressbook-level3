04742781e4e7fa3078481b390e767094baa6d3e3
Amro Shohoud <amro@74-217.priv23.nus.edu.sg>
2017-03-01 12:53:21 +0800

    Use Dependency Injection

--- src/seedu/addressbook/storage/Storage.java
+++ src/seedu/addressbook/storage/Storage.java
@@ -4,11 +4,13 @@ import seedu.addressbook.data.AddressBook;
 import seedu.addressbook.storage.StorageFile.StorageOperationException;
 
 /**
- * Abstract class for addressbook storage.
+ * Abstract class for address book storage.
  */
 public interface Storage {
 
-    public void save(AddressBook addressBook) throws StorageOperationException;
-    public AddressBook load() throws StorageOperationException;
+    public void save(AddressBook addressBook)
+            throws StorageOperationException, seedu.addressbook.storage.StorageStub.StorageOperationException;
+    public AddressBook load()
+            throws StorageOperationException, seedu.addressbook.storage.StorageStub.StorageOperationException;
     public String getPath();
 }
