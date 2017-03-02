a037221939095dc038a262f19ecc63638d868ac5
Chan Lup Seng <lupsengchan@gmail.com>
2017-03-02 16:56:32 +0800

    Add StorageStub for LogicTest. Dependency Injection

---
+++ src/seedu/addressbook/storage/StorageStub.java
@@ -0,0 +1,30 @@
+package seedu.addressbook.storage;
+
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.storage.StorageFile.StorageOperationException;
+
+/**
+ * Represents the file used to store address book data.
+ */
+public class StorageStub implements Storage {
+
+	public StorageStub(String path) {
+	}
+
+	@Override
+	public void save(AddressBook addressBook) throws StorageOperationException {
+		
+	}
+
+	@Override
+	public AddressBook load() throws StorageOperationException {
+		return null;
+	}
+
+	@Override
+	public String getPath() {
+		return null;
+	}
+
+
+}
