b2717c0dcc09e9c9bb5e81b2f1b5402e4c92de73
mervynyeezl <mervynyeezl@gmail.com>
2017-03-02 14:46:45 +0800

    Make StorageFile and Logic depend on abstract class Storage

---
+++ src/seedu/addressbook/storage/Storage.java
@@ -0,0 +1,10 @@
+package seedu.addressbook.storage;
+
+import seedu.addressbook.storage.StorageFile.StorageOperationException;
+import seedu.addressbook.data.AddressBook;
+
+public abstract class Storage {
+	public abstract void save(AddressBook addressBook) throws StorageOperationException;
+	public abstract AddressBook load() throws StorageOperationException;
+	public abstract String getPath();
+}
