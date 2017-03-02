40dbe2dfc94d85bcebdb6c689d94278dbfba8253
LEUNG Cheuk Ting <e0148074@u.nus.edu>
2017-03-03 01:28:21 +0800

    T6A1

---
+++ src/seedu/addressbook/storage/Storage.java
@@ -0,0 +1,24 @@
+package seedu.addressbook.storage;
+
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.storage.StorageFile.StorageOperationException;
+
+public abstract class Storage {
+
+
+    public static class InvalidStorageFilePathException extends IllegalValueException {
+        public InvalidStorageFilePathException(String message) {
+            super(message);
+        }
+    }
+	
+    // add all the abstract function
+    
+	public abstract void save(AddressBook addressBook) throws StorageOperationException;
+	
+	public abstract AddressBook load() throws StorageOperationException;
+	
+	public abstract String getPath();
+
+}
