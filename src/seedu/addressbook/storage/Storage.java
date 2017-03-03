80f68b0146fe25ba0b56f84b20eb0b8d989f7419
Tu An Huynh Van <arishuynhvan@gmail.com>
2017-03-03 17:59:15 +0800

    Apply Dependency Inversion Principle [LO-DIP]

---
+++ src/seedu/addressbook/storage/Storage.java
@@ -0,0 +1,30 @@
+package seedu.addressbook.storage;
+
+import seedu.addressbook.data.AddressBook;
+import seedu.addressbook.data.exception.IllegalValueException;
+
+public interface Storage {
+
+    /**
+     * Signals that the given file path does not fulfill the storage filepath constraints.
+     */
+    public class InvalidStorageFilePathException extends IllegalValueException {
+		public InvalidStorageFilePathException(String message) {
+			super(message);
+			// TODO Auto-generated constructor stub
+		}}
+    /**
+     * Signals that some error has occured while trying to convert and read/write data between the application
+     * and the storage file.
+     */
+    public static class StorageOperationException extends Exception {
+        public StorageOperationException(String message) {
+            super(message);
+        }
+    }
+    public String getPath();
+    public void save(AddressBook addressBook)throws StorageOperationException;
+    public AddressBook load() throws StorageOperationException ;
+    public Storage newStorage() throws InvalidStorageFilePathException;
+
+}
