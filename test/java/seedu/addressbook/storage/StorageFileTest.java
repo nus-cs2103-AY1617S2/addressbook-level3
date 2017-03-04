712fd5840f3235e63ce34286285e2c21c0684ca1
VeryLazyBoy <frank16874@gmail.com>
2017-03-04 19:10:47 +0800

    Update StorageFileTest

--- test/java/seedu/addressbook/storage/StorageFileTest.java
+++ test/java/seedu/addressbook/storage/StorageFileTest.java
@@ -17,7 +17,6 @@ import seedu.addressbook.data.person.Person;
 import seedu.addressbook.data.person.Phone;
 import seedu.addressbook.data.tag.Tag;
 import seedu.addressbook.data.tag.UniqueTagList;
-import seedu.addressbook.storage.StorageFile.StorageOperationException;
 import static seedu.addressbook.util.TestUtil.assertTextFilesEqual;
 
 public class StorageFileTest {
@@ -45,7 +44,7 @@ public class StorageFileTest {
     public void load_invalidFormat_exceptionThrown() throws Exception {
         // The file contains valid xml data, but does not match the AddressBook class
         StorageFile storage = getStorage("InvalidData.txt");
-        thrown.expect(StorageOperationException.class);
+        thrown.expect(seedu.addressbook.storage.Storage.StorageOperationException.class);
         storage.load();
     }
 
