d92b826f79cc5593068ee3d79f5b24946166cd59
Chengcheng Li <lcc199533@gmail.com>
2017-03-05 10:43:00 +0800

    Add header comment to Storage.java

--- src/seedu/addressbook/storage/Storage.java
+++ src/seedu/addressbook/storage/Storage.java
@@ -3,6 +3,9 @@ package seedu.addressbook.storage;
 import seedu.addressbook.data.AddressBook;
 import seedu.addressbook.storage.StorageFile.StorageOperationException;
 
+/**
+ * Represents the file used to store address book data.
+ */
 public abstract class Storage {
 	public abstract AddressBook load() throws StorageOperationException;
 	public abstract void save(AddressBook addressBook) throws StorageOperationException;
